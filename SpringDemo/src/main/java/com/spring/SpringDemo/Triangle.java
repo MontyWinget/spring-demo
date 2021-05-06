9102bb0c9bed9546722110dc3ac627767a82c189
Monty Winget <monty.winget@gmail.com>
2021-05-06 12:26:53 +0530

    Triangle and Point class example

---
+++ SpringDemo/src/main/java/com/spring/SpringDemo/Triangle.java
@@ -0,0 +1,42 @@
+package com.spring.SpringDemo;
+
+public class Triangle {
+	
+	private	Point pointA;
+	private	Point pointB;
+	private	Point pointC;
+	
+	
+	
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	public void draw() {
+		System.out.println("Traingle is drawn");
+		System.out.println("Piont A (" + getPointA().getX() + "," + getPointA().getY() + ")");
+		System.out.println("Piont B (" + getPointB().getX() + "," + getPointB().getY() + ")");
+		System.out.println("Piont C (" + getPointC().getX() + "," + getPointC().getY() + ")");
+	}
+
+}
