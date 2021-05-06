9c21cca086473636666672cd951aaca02ae50ca6
Monty Winget <monty.winget@gmail.com>
2021-05-06 12:23:30 +0530

    Main app java file

--- SpringDemo/src/main/java/com/spring/SpringDemo/App.java
+++ SpringDemo/src/main/java/com/spring/SpringDemo/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Triangle traingle = new Triangle();
+        traingle.draw();
+        
     }
 }
